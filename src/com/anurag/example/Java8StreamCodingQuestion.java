f62037b0418dbc34cf33ddbc37eff6ee77dd1088
Anurag upadhyay <anuragupadhyayblogs@gmail.com>
2022-12-12 17:17:34 +0530

    few more example added

--- src/com/anurag/example/Java8StreamCodingQuestion.java
+++ src/com/anurag/example/Java8StreamCodingQuestion.java
@@ -1,28 +1,63 @@
 package com.anurag.example;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Java8StreamCodingQuestion {
 	public static void main(String[] args) {
-		List<Integer> asList = Arrays.asList(1, 17, 58, 29, 5, 2, 36, 24, 28, 78, 98);
-		
+		List<Integer> asList = Arrays.asList(1, 17, 78, 29, 5, 2, 36, 24, 28, 89, 98);
+
 		// Given a list of numbers, return the sum of all numbers
-		Optional<Integer> sumReduce = asList.stream().reduce((a,b)->a+b);
+		Optional<Integer> sumReduce = asList.stream().reduce((a, b) -> a + b);
 		System.out.println("Sum is : " + sumReduce.get());
-	
+
 		// Given a list of numbers, return the average of all numbers
-		double orElse = asList.stream().mapToInt(a->a).average().orElse(0);
+		double orElse = asList.stream().mapToInt(a -> a).average().orElse(0);
 		System.out.println("Average of all number : " + orElse);
+
+		// Given a list of numbers, square them and filter the number which are greater
+		// 100 and then find the average of them.
+		double orElse2 = asList.stream().map(a -> a * a).filter(a -> a > 100).mapToInt(a -> a).average().orElse(0);
+		System.out.println("Average of number greater than 100 : " + orElse2);
+
+		// Given a list of numbers, find out all the numbers starting with 2
+		List<Integer> collect = asList.stream().map(num -> String.valueOf(num)).filter(n -> n.startsWith("2"))
+				.map(Integer::valueOf).collect(Collectors.toList());
+		System.out.println("Number start with 2 in list : " + collect);
+
+		// Given a list of number find the duplicate
+		asList.stream().filter(f -> Collections.frequency(asList, f) > 1).forEach(System.out::print);
+		//To print distinct
+		asList.stream().distinct().forEach(System.out::println);
 		
-		//Given a list of numbers, square them and filter the number which are greater 100 and then find the avarage of them.
-		double orElse2 = asList.stream().map(a->a*a).filter(a->a>100).mapToInt(a->a).average().orElse(0);
-		System.out.println("Average of number greater than 100 : "+orElse2);
+		//Given a list of numbers, print the maximum and minimum values
+		Integer max = asList.stream().max(Comparator.comparing(Integer::valueOf)).get();
+		System.out.println("Max : " +  max);
 		
-		//Given a list of numbers, find out all the numbers starting with 2
-		List<Integer> collect = asList.stream().map(num->String.valueOf(num)).filter(n->n.startsWith("2")).map(Integer::valueOf).collect(Collectors.toList());
-		System.out.println("Number start with 2 in list : "+ collect);
+		Integer min = asList.stream().min(Comparator.comparing(Integer::valueOf)).get();
+		System.out.println("Min : " +  min);
+		
+		//Given list of number, sort them in ASC and DESC order and print 
+		asList.stream().sorted().forEach(System.out::print);
+		asList.stream().sorted(Comparator.reverseOrder()).forEach(System.out::print);
+		
+		//Given list of numbers, return the first 5 element and sum.
+		// Approach we can use limit()followed by reduce()
+		 
+		Optional<Integer> collect2 = asList.stream().limit(5).reduce((a,b)->a+b);
+		System.out.println("Sum : " + collect2.get());
+		
+		// Given a list of numbers. skip the first 5 numbers and return the sum of remaining numbers
+		Integer sum = asList.stream().skip(5).reduce((a,b)->a+b).get();
+		System.out.println(" Sum : "+ sum);
+		
+		
+		
+		
+
 	}
 }
